1e04d41d77ca32975d37923887aa1895e0a98a21
Roman Shustrov <r.shustrov@4slovo.ru>
2018-06-01 13:51:09 +0300

    point

---
+++ CodeGenerator/interfaceList/ConfigInterfaceListTreeGenerator.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: r.shustrov
+ * Date: 6/1/2018
+ * Time: 12:59 PM
+ */
+
+namespace YamlConfig\InterfaceListCodeGenerator;
+
+
+use YamlConfig\StructureCodeGenerator\ConfigStructureGeneratorInterface;
+use YamlConfig\StructureCodeGenerator\ConfigStructureInfoInterface;
+use YamlConfig\StructureCodeGenerator\ConfigStructureTreeGenerator;
+use YamlConfig\StructureCodeGenerator\StructureInfoListInterface;
+
+class ConfigInterfaceListTreeGenerator extends ConfigStructureTreeGenerator
+{
+
+    protected function createStructureInfoList()
+    {
+        // TODO: Implement createStructureInfoList() method.
+    }
+
+    protected function createConfigStructureGenerator($configStructureInfo)
+    {
+        // TODO: Implement createConfigStructureGenerator() method.
+    }
+}
